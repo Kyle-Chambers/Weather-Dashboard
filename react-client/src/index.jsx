62b468a78866dcd85c189c629c428cf2c496468b
Kyle Chambers <kylejchambers@gmail.com>
2018-03-31 23:08:19 -0700

    basic app rendering properly, get request to openWeatherMap working

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,34 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import List from './components/List.jsx';
+import getLatitudeAndLongitude from '../helpers/getLatitudeAndLongitude.js'
+import { 
+  getWeatherByZip, 
+  getWeatherByLatitudeAndLongitude 
+} from '../helpers/getWeather.js';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      items: []
+      items: [],
+      weatherInfo: {},
+      latAndLong: {
+        latitude: null, 
+        longitude: null
+      },
     }
   }
 
   componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
+
+    getLatitudeAndLongitude()
+    .then((position) => {
+      this.setState({
+        latAndLong: position
+      });
+    })
+    .then(() => {
+      let { latitude, longitude } = this.state.latAndLong
+      getWeatherByLatitudeAndLongitude(latitude, longitude)
+      .then((info => {
+        console.log(info.data);
+      }))
+    })
+    .catch((err) => {
+      console.log(err);
     });
+
   }
 
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+    return (
+      <div>
+        <h1>Item List</h1>
+        <p>{this.state.latAndLong.latitude}</p>
+        <List items={this.state.items}/>
+      </div>
+    )
   }
 }
 
