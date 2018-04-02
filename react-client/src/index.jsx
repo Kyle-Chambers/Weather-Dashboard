8534b1f171b37a88fafe1490dbcbe18f15a291ea
Kyle Chambers <kylejchambers@gmail.com>
2018-04-01 18:04:44 -0700

    Displaying content correctly

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,140 +2,169 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import List from './components/List.jsx';
-import getLatitudeAndLongitude from '../helpers/getLatitudeAndLongitude.js'
+import getLatitudeAndLongitude from '../helpers/getLatitudeAndLongitude.js';
+import style from './styles/main.css'
 import { 
   getWeatherByZip, 
   getWeatherByLatitudeAndLongitude 
 } from '../helpers/getWeather.js';
 
 
-const iconStyle = {
-  fontSize: '200px',
-  textAlign: 'center',
-  color: 'grey'
-};
 
-class WeatherAtLocation extends React.Component {
+class LocalWeather extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
       // defaultWeatherInfo: {},
       latitude: 'loading', 
       longitude: 'loading',
-      temperature: 'loading',
+      temperature: 0,
       humidity: 'loading',
       location: 'loading',
-      city: 'loading',
+      country: 'loading',
       windSpeed: 'loading',
-      iconId: null
+      iconId: null,
+      descriptor: 'loading'
     }
+  }
+
+  capitalizeDescriptor() {
+
   }
 
   componentDidMount() {
 
-    getLatitudeAndLongitude()
-    .then((position) => {
-      let { latitude, longitude } = position;
-      this.setState({
-        latitude,
-        longitude
-      });
-    })
-    .then(() => {
-
-      getWeatherByLatitudeAndLongitude(this.state.latitude, this.state.longitude)
-
-      .then((info => {
-        console.log(info);
-
-        let temperature = info.data.main.temp;
-        let humidity = info.data.main.humidity;
-        let location = info.data.name;
-        let city = info.data.sys.country;
-        let windSpeed = info.data.wind.speed;
-        let iconId = info.data.weather[0].id;
-
-        console.log()
-
-        this.setState({
-          temperature,
-          humidity,
-          location,
-          city,
-          windSpeed,
-          iconId
-        })
-
-        console.log(this.state)
-      }))
-      .catch((err) => {
-        console.log(err);
-      });
-
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
-    ////////// <<<<<<<<<<<< HARD CODED LAT AND LONG UNCOMMENT CODE ABOVE TO GO BACK >>>>>>>>>>>>
-
-    // let { latitude, longitude } =  {latitude: 37.7771726, longitude: -122.4238155};
-
-    // this.setState({
-    //   latitude,
-    //   longitude
-    // })
-
-    // getWeatherByLatitudeAndLongitude(latitude, longitude)
-    // .then((info => {
-    //   console.log(info);
-
-    //   let temperature = info.data.main.temp;
-    //   let humidity = info.data.main.humidity;
-    //   let location = info.data.name;
-    //   let city = info.data.sys.country;
-    //   let windSpeed = info.data.wind.speed;
-    //   let iconId = info.data.weather[0].id;
-
-    //   console.log(iconId);
-
+    // getLatitudeAndLongitude()
+    // .then((position) => {
+    //   let { latitude, longitude } = position;
     //   this.setState({
-    //     temperature,
-    //     humidity,
-    //     location,
-    //     city,
-    //     windSpeed,
-    //     iconId
-    //   })
+    //     latitude,
+    //     longitude
+    //   });
+    // })
+    // .then(() => {
 
-    //   console.log(this.state)
-    // }))
+    //   getWeatherByLatitudeAndLongitude(this.state.latitude, this.state.longitude)
+
+    //   .then((info => {
+    //     console.log(info);
+
+    //     let temperature = info.data.main.temp;
+    //     let humidity = info.data.main.humidity;
+    //     let location = info.data.name;
+    //     let city = info.data.sys.country;
+    //     let windSpeed = info.data.wind.speed;
+    //     let iconId = info.data.weather[0].id;
+
+    //     this.setState({
+    //       temperature,
+    //       humidity,
+    //       location,
+    //       city,
+    //       windSpeed,
+    //       iconId
+    //     })
+
+    //   }))
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
+    // })
     // .catch((err) => {
     //   console.log(err);
     // });
+
+    ////////// <<<<<<<<<<<< HARD CODED LAT AND LONG UNCOMMENT CODE ABOVE TO GO BACK >>>>>>>>>>>>
+
+    let { latitude, longitude } =  {latitude: 37.7771726, longitude: -122.4238155};
+
+    this.setState({
+      latitude,
+      longitude
+    })
+
+    getWeatherByLatitudeAndLongitude(latitude, longitude)
+    .then((info => {
+      console.log(info);
+
+      let temperature = Math.floor(info.data.main.temp);
+      let humidity = info.data.main.humidity;
+      let location = info.data.name;
+      let country = info.data.sys.country;
+      let windSpeed = info.data.wind.speed;
+      let iconId = info.data.weather[0].id;
+      let descriptor = info.data.weather[0].description.split(' ').map((word) => word[0].toUpperCase() + word.slice(1)).join(' ');
+
+      this.setState({
+        temperature,
+        humidity,
+        location,
+        country,
+        windSpeed,
+        iconId,
+        descriptor
+      })
+
+    }))
+    .catch((err) => {
+      console.log(err);
+    });
 
   }
 
   render () {
     return (
-      <div>
+      <div className={style.main}>
 
-        <h1>Weather</h1>
-        <i style={iconStyle} className={`wi wi-owm-${this.state.iconId}`}></i>
+        {/* main info<> */}
 
-        <p>{this.state.iconId}</p>
-        <p> Location: {this.state.location}  </p>
-        <p> Location: {this.state.city} </p>
-        <p> Temperature: {this.state.temperature}</p>
-        <p> Humidity: {this.state.humidity}</p>
-        <p> Wind Speed: {this.state.windSpeed}</p>
-        <p> Your latitude: {Math.floor(this.state.latitude)}</p>
-        <p> Your longitude: {Math.floor(this.state.longitude)}</p>
+        <div className={style.mainInfo}> 
+
+          <div className={style.iconContainer}>
+            <i  className={[style.icon,` wi wi-owm-${this.state.iconId}`].join(' ')}></i> 
+          </div>
+
+          <p className={style.temperature}> 
+            {`${this.state.temperature}°`}
+          </p>
+
+        </div>
+
+        {/* <>detailed info<> */}
+
+        <div className={style.detailedInfo}>
+
+          <p className={style.descriptor}>
+            {this.state.descriptor}
+          </p>
+
+          <p className={style.location}> 
+            City | {this.state.location}
+          </p>
+
+          <p className={style.country}> 
+            Country | {this.state.country}
+          </p>
+
+          <p className={style.coordinates}> 
+            Coordinates | {this.state.latitude.toString().slice(0,4)}, {this.state.longitude.toString().slice(0,4)}
+          </p>
+
+          <p className={style.humidity}> 
+            Humidity | {this.state.humidity}
+          </p>
+
+          <p className={style.windSpeed}> 
+            Wind Speed | {this.state.windSpeed}
+          </p>
+
+        </div>
 
       </div>
     )
   }
 }
 
-ReactDOM.render(<WeatherAtLocation />, document.getElementById('app'));
+ReactDOM.render(<LocalWeather />, document.getElementById('app'));
 
